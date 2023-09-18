2cd7f75a1f0d8aad765878d2bf5039d445f976a4
Simon Jinlin Cen <simoncen2@gmail.com>
2023-09-18 19:20:17 -0400

    added the email box and added all the checks for checking whether the email address is valid and whether it is an uoft email address

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email
 from flask_moment import Moment
 from datetime import datetime
 
@@ -12,8 +12,14 @@ moment = Moment(app)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+def custom_email_validator(form, field):
+    if '@' not in form.email.data:
+        raise ValidationError(f"Please include an '@' in the email address. '{field.data}' is missing an '@'.")
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', 
+                            validators=[DataRequired(), custom_email_validator])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,15 +27,31 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        if 'utoronto' in form.email.data:
+            session['isUofTEmail'] = True
+        else:
+            session['isUofTEmail'] = False
+
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    
+    return render_template('index.html', 
+                            form=form, name=session.get('name'), 
+                            email=session.get('email'),
+                            isUofTEmail=session.get('isUofTEmail'))
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name, current_time=datetime.utcnow())
+    return render_template('user.html',
+                            name=name, 
+                            current_time=datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
