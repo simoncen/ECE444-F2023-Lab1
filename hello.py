d4a46e6c6f740dcc4815f02c49d05c5b28638213
Simon Jinlin Cen <simoncen2@gmail.com>
2023-09-19 00:20:22 -0400

    changed the sixth screenshot

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email
+from wtforms.validators import DataRequired, Email
 from flask_moment import Moment
 from datetime import datetime
 
@@ -12,14 +12,10 @@ moment = Moment(app)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
 
-def custom_email_validator(form, field):
-    if '@' not in form.email.data:
-        raise ValidationError(f"Please include an '@' in the email address. '{field.data}' is missing an '@'.")
-
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     email = StringField('What is your UofT Email address?', 
-                            validators=[DataRequired(), custom_email_validator])
+                            validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
